6f2a85e47a0219ff383e718c6eb51906b7df552b
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-05-19 12:09:34 +0700

    Refactor Helm upgrade command for Prometheus
    Updated Helm command to use the Prometheus community repository and adjusted syntax for multi-line commands.

--- monitor-stack/run-central.sh
+++ monitor-stack/run-central.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-helm upgrade --install monitoring kube-prometheus-stack \
-  -n prometheus \
-  --create-namespace \
-  -f values-central.yaml
+
   
- #kubectl apply -f prometheus-nodeport.yaml
+
+
+
+# Thêm Prometheus community repository
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# Install với values-central.yaml của bạn
+helm upgrade --install monitoring prometheus-community/kube-prometheus-stack `
+  -n prometheus `
+  --create-namespace `
+  -f values-central.yaml
+
+
+#kubectl apply -f prometheus-nodeport.yaml
